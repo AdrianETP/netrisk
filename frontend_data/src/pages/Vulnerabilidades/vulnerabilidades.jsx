c5cbc73990be6299b94aba607159d2f2535490f4
Jinellie <89491839+Jinellie@users.noreply.github.com>
2024-10-29 18:46:16 -0600

    pantalla vulnerabilidades

--- frontend_data/src/pages/Vulnerabilidades/vulnerabilidades.jsx
+++ frontend_data/src/pages/Vulnerabilidades/vulnerabilidades.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ProgressCircle from "../Components/ProgressCircle.jsx";
 import EditableTable from "../Components/EditableTable.jsx";
 import { Button, ButtonGroup } from "@nextui-org/button";
@@ -9,34 +9,40 @@ import WarningRoundedIcon from "@mui/icons-material/WarningRounded";
 import RemoveCircleOutlineRoundedIcon from "@mui/icons-material/RemoveCircleOutlineRounded";
 import CheckCircleOutlineRoundedIcon from "@mui/icons-material/CheckCircleOutlineRounded";
 import { get } from "../../ApiRequests.js";
-import { useEffect } from "react";
-
 
 function Vulnerabilidades() {
-const [initialDataTable1, setInitialDataTable1] = useState([]);
-const [isLoading, setIsLoading] = useState(true);
+	const [initialDataTable1, setInitialDataTable1] = useState([]);
+	const [initialDataTable2, setInitialDataTable2] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [table1Loaded, setTable1Loaded] = useState(false);
+	const [table2Loaded, setTable2Loaded] = useState(false);
+	const [displayValue, setDisplayValue] = useState("0");
+	const [totalVulnerabilities, setTotalVulnerabilities] = useState(0);
+	const [stats, setStats] = useState([]);
 
-useEffect(() => {
-	get("api/vul-tec")
-		.then((result) => {
-			setInitialDataTable1(result.data);
-			setIsLoading(false);
-			console.log(result.data);
-		})
-		.catch((error) => {
-			console.error("Ocurrió un error:", error);
-			setIsLoading(false);
+	// Función para ordenar vulnerabilidades
+	const sortVulnerabilities = (vulnerabilities) => {
+		return vulnerabilities.sort((a, b) => {
+			const impactOrder = {
+				Crítico: 1,
+				Alto: 2,
+				Moderado: 3,
+				Bajo: 4,
+			};
+			// Primero ordena por impacto, luego por pérdida potencial en orden descendente
+			if (impactOrder[a.impact] !== impactOrder[b.impact]) {
+				return impactOrder[a.impact] - impactOrder[b.impact];
+			} else {
+				return b.potentialLoss - a.potentialLoss;
+			}
 		});
-}, []);
-	
-const [initialDataTable2, setInitialDataTable2] = useState([]);
+	};
 
 	useEffect(() => {
-		get("api/vul-org")
+		get("api/vul-tec")
 			.then((result) => {
-				setInitialDataTable2(result.data);
-				setIsLoading(false);
-				console.log(result.data);
+				setInitialDataTable1(sortVulnerabilities(result.data));
+				setTable1Loaded(true);
 			})
 			.catch((error) => {
 				console.error("Ocurrió un error:", error);
@@ -44,30 +50,64 @@ const [initialDataTable2, setInitialDataTable2] = useState([]);
 			});
 	}, []);
 
+	useEffect(() => {
+		get("api/vul-org")
+			.then((result) => {
+				setInitialDataTable2(sortVulnerabilities(result.data));
+				setTable2Loaded(true);
+			})
+			.catch((error) => {
+				console.error("Ocurrió un error:", error);
+				setIsLoading(false);
+			});
+	}, []);
+
+	useEffect(() => {
+		if (table1Loaded && table2Loaded) {
+			setIsLoading(false);
+			const totalVulnerabilities =
+				initialDataTable1.length + initialDataTable2.length;
+			const mitigationPercentage =
+				(initialDataTable2.length / totalVulnerabilities) * 100;
+			setDisplayValue(mitigationPercentage.toFixed(2));
+			setTotalVulnerabilities(totalVulnerabilities);
+
+			const severityCounts = { critical: 0, high: 0, medium: 0, low: 0 };
+
+			[...initialDataTable1, ...initialDataTable2].forEach((vul) => {
+				if (vul.impact === "Crítico") severityCounts.critical += 1;
+				else if (vul.impact === "Alto") severityCounts.high += 1;
+				else if (vul.impact === "Moderado") severityCounts.medium += 1;
+				else if (vul.impact === "Bajo") severityCounts.low += 1;
+			});
+
+			setStats([
+				{
+					icon: <ErrorOutlinedIcon style={{ fontSize: 24 }} />,
+					value: severityCounts.critical,
+					color: "#F31260",
+				},
+				{
+					icon: <WarningRoundedIcon style={{ fontSize: 24 }} />,
+					value: severityCounts.high,
+					color: "#F5A524",
+				},
+				{
+					icon: <RemoveCircleOutlineRoundedIcon style={{ fontSize: 24 }} />,
+					value: severityCounts.medium,
+					color: "#7828C8",
+				},
+				{
+					icon: <CheckCircleOutlineRoundedIcon style={{ fontSize: 24 }} />,
+					value: severityCounts.low,
+					color: "#17C964",
+				},
+			]);
+		}
+	}, [table1Loaded, table2Loaded, initialDataTable1, initialDataTable2]);
+
 	const [selectedTable, setSelectedTable] = useState("table1");
 
-	const stats = [
-		{
-			icon: <ErrorOutlinedIcon style={{ fontSize: 24 }} />,
-			value: 12,
-			color: "#F31260",
-		},
-		{
-			icon: <WarningRoundedIcon style={{ fontSize: 24 }} />,
-			value: 25,
-			color: "#F5A524",
-		},
-		{
-			icon: <RemoveCircleOutlineRoundedIcon style={{ fontSize: 24 }} />,
-			value: 7,
-			color: "#7828C8",
-		},
-		{
-			icon: <CheckCircleOutlineRoundedIcon style={{ fontSize: 24 }} />,
-			value: 7,
-			color: "#17C964",
-		},
-	];
 	const columnsTable1 = [
 		{ key: "id", label: "ID DEL ACTIVO" },
 		{ key: "vulnerability", label: "VULNERABILIDAD" },
@@ -75,7 +115,6 @@ const [initialDataTable2, setInitialDataTable2] = useState([]);
 		{ key: "potentialLoss", label: "PÉRDIDA POTENCIAL" },
 		{ key: "impact", label: "IMPACTO" },
 	];
-
 
 	const columnsTable2 = [
 		{ key: "email", label: "CORREO" },
@@ -87,9 +126,7 @@ const [initialDataTable2, setInitialDataTable2] = useState([]);
 
 	const editableColumns = [""];
 
-	const dropdownOptions = {
-		
-	};
+	const dropdownOptions = {};
 
 	return (
 		<div className="">
@@ -97,16 +134,16 @@ const [initialDataTable2, setInitialDataTable2] = useState([]);
 				<div className="flex flex-row px-4 circulos-vuln">
 					<div className="flex flex-row p-4 gap-4 mb-2">
 						<ProgressCircle
-							progressValue={100} // Controls the progress bar percentage
-							displayValue="100" // The value shown inside the circle
+							progressValue={100}
+							displayValue={totalVulnerabilities.toString()}
 							customColor="#0DD4CE"
 							size={170}
 							strokeWidth={9}
 							label="Vulnerabilidades detectadas"
 						/>
 						<ProgressCircle
-							progressValue={75} // Controls the progress bar percentage
-							displayValue="75%" // The value shown inside the circle
+							progressValue={displayValue}
+							displayValue={`${displayValue}%`}
 							customColor="#0DD4CE"
 							size={170}
 							strokeWidth={9}
@@ -124,8 +161,8 @@ const [initialDataTable2, setInitialDataTable2] = useState([]);
 						onClick={() => setSelectedTable("table1")}
 						className={`px-4 py-2 text-sm ${
 							selectedTable === "table1"
-								? "bg-[#A1A1AA] text-[#202020] font-semibold" // Selected: Light grey background, dark text
-								: "bg-transparent text-[#A1A1AA] border-[#A1A1AA] border-2" // Unselected: Transparent background, grey border and text
+								? "bg-[#A1A1AA] text-[#202020] font-semibold"
+								: "bg-transparent text-[#A1A1AA] border-[#A1A1AA] border-2"
 						}`}
 					>
 						Vulnerabilidades técnicas
@@ -134,8 +171,8 @@ const [initialDataTable2, setInitialDataTable2] = useState([]);
 						onClick={() => setSelectedTable("table2")}
 						className={`px-4 py-2 text-sm ${
 							selectedTable === "table2"
-								? "bg-[#A1A1AA] text-[#202020] font-semibold" // Selected: Light grey background, dark text
-								: "bg-transparent text-[#A1A1AA] border-[#A1A1AA] border-2" // Unselected: Transparent background, grey border and text
+								? "bg-[#A1A1AA] text-[#202020] font-semibold"
+								: "bg-transparent text-[#A1A1AA] border-[#A1A1AA] border-2"
 						}`}
 					>
 						Vulnerabilidades organizacionales
